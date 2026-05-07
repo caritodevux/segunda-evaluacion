1d6fb81997620da432db359eab844cc7ed9f6213
caritodevux <c.rehbeindelrio@gmail.com>
2026-05-07 19:54:21 -0400

    requerimientos de la evaluacion

---
+++ script.js
@@ -0,0 +1,40 @@
+//Arreglo inicial de usuarios
+let usuarios =[];
+
+//funciones
+
+agregarUsuario () {
+    /* 
+    1. Nombre
+    2. Edad
+    3. Rol Admin o Usuario
+    4. Estado
+    Validar al presionar boton
+    Ningún campo puede estar vacío.
+    Edad debe ser un número mayor a 0 */
+};
+
+mostrarUsuario () {
+    /**
+     * Usuario debe mostrar:
+     * - Nombre
+     * - Edad
+     * - Rol
+     * - Estado
+     * Debe modificarse, llamar a cambiarEstado()?
+     */
+};
+
+filtrarUsuario () {
+    //Selector: Todos, soloAdmins y soloUsuarios
+};
+
+cambiarEstado () {
+    //Alterna estado de activo a inactivo
+};
+
+validarFormulario () {
+    /*Dentro de agregarUsuario?
+    Ningún campo puede estar vacío.
+    Edad debe ser un número mayor a 0. */
+};
